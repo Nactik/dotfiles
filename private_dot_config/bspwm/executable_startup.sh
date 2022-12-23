0df9a4ab2e1f8d5b6d257f5fbd9a0cb73e03081e
Nathan Capiaux <nathan.capiaux@sia-partners.com>
2022-12-23 13:04:57 +0100

    Better external monitor setup + polybar

--- private_dot_config/bspwm/executable_startup.sh
+++ private_dot_config/bspwm/executable_startup.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-internal_monitor=eDP-1
-external_monitor=DP-2
+internal_monitor="eDP-1"
+external_monitor="DP-2"
 
 monitor_add() {
 
-  desktops=4 # How many desktopo to move to 2nd monitor
+  desktops=5 # How many desktopo to move to 2nd monitor
+
+  bspc monitor "$external_monitor" -a Desktop
   
-  for desktop in $(bspc query -D -m $internal_monitor | sed "$desktops"q)
-  do
-    bspc desktop $desktop --to-monitor $external_monitor
+  for desktop in $(bspc query -D --names -m "$internal_monitor" | sed "$desktops"q); do
+    bspc desktop "$desktop" --to-monitor "$external_monitor"
   done
 
   bspc desktop Desktop --remove
+  bspc wm -O "$external_monitor" "$internal_monitor"
 }
 
 
 monitor_remove() {
   # Temp desktop because one desktop is require per monitor
-  bspc monitor $internal_monitor -a Desktop
+  # bspc monitor $internal_monitor -a Desktop
   
-  for desktop in $(bspc query -D -m $internal_monitor)
-  do
-    bspc desktop $desktop --to-monitor $external_monitor
-  done
+  # for desktop in $(bspc query -D -m $internal_monitor)
+  # do
+  #  bspc desktop $desktop --to-monitor $external_monitor
+  # done
+  
+  bspc monitor "$external_monitor" -a Desktop
 
-  bspc monitor $external_monitor -a Desktop
-
-  for desktop in $(bspc query -D -m $external_monitor)
-  do
-    bspc desktop $desktop --to-monitor $internal_monitor
+  for desktop in $(bspc query -D -m "$external_monitor"); do
+    bspc desktop "$desktop" --to-monitor "$internal_monitor"
   done 
 
   bspc desktop Desktop --remove
+  bspc monitor "$internal_monitor" -o 1 2 3 4 5 6 7 8 9 0
+
 }
 
 ## Main setup
