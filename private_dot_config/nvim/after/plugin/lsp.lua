d2e33d5609dd3c73d5f12797c2d2276e4b6be776
Nathan Capiaux <capiauxnathan@gmail.com>
2023-03-27 00:20:19 +0200

    Better lsp config with diagnostics

--- private_dot_config/nvim/after/plugin/lsp.lua
+++ private_dot_config/nvim/after/plugin/lsp.lua
@@ -28,4 +28,13 @@ lsp.ensure_installed({
 lsp.nvim_workspace()
 lsp.setup()
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
 
