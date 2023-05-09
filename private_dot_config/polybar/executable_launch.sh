812bebdfdbfdc696b1ef65b343e8b89521c446a3
Nathan Capiaux <capiauxnathan@gmail.com>
2023-05-09 22:50:55 +0200

    access config via absolute path

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -12,11 +12,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -c ~/.config/polybar/config.ini top 2>~/.config/polybar/top.log & 
 
 if [[ ! -z ${external_monitor} ]]; then
-  last_monitor=$(cat .config/polybar/last_monitor.txt | head -n 1)
+  last_monitor=$(cat ~/.config/polybar/last_monitor.txt | head -n 1)
   ## Replace last_monitor by current in polybar config
   sed -i "/eDP/! s/${last_monitor}/${external_monitor}/g" ~/.config/polybar/config.ini
   ## Update last_monitor
-  echo $external_monitor > .config/polybar/last_monitor.txt
+  echo $external_monitor > ~/.config/polybar/last_monitor.txt
 
   polybar -c ~/.config/polybar/config.ini external 2>~/.config/polybar/external.log & 
 fi
