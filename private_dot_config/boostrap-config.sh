707d9ed9d6ef1864fa2d2f46bfdeaa7360b7cdb7
Nathan Capiaux <capiauxnathan@gmail.com>
2024-05-12 21:52:59 +0200

    Bootstrap file

---
+++ private_dot_config/boostrap-config.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+echo "Upgrading sources ..."
+sudo apt upgrade -y;
+
+echo "Installing common utils"
+sudo apt install -y curl \
+  git \
+  gcc \
+  yq \
+  jq \
+  google-chrome-stable \
+  alacritty \
+  bspwm \
+  rofi \
+  polybar \
+  dunst \
+
+
+# i3-color dependencies
+echo "Installing i3-color build deps ..."
+sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+
+echo "Installing brew ..."
+
+# Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
+/home/linuxbrew/.linuxbrew/bin/brew tap Homebrew/bundle
+
+## Chezmoi
+echo "Installing and configuring chezmoi .."
+/home/linuxbrew/.linuxbrew/bin/brew install chezmoi
+
+/home/linuxbrew/.linuxbrew/bin/chezmoi init https://github.com/Nactik/dotfiles
+/home/linuxbrew/.linuxbrew/bin/chezmoi apply ~/.zshrc
+/home/linuxbrew/.linuxbrew/bin/chezmoi apply ~/.config/
+/home/linuxbrew/.linuxbrew/bin/chezmoi apply ~/.local
+/home/linuxbrew/.linuxbrew/bin/chezmoi apply ~/.fonts
+
+# Homebrew 
+/home/linuxbrew/.linuxbrew/bin/brew bundle --file ~/.config/Brewfile
+
+# Packer
+echo "Installing packer ..."
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Zsh auto-suggestion
+echo "Installing zsh auto-suggestion"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install Rust
+echo "Installing Rust ..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Installing Golang ..."
+wget -P https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.22.3.linux-amd64.tar.gz
+
+echo "Installing nvm ..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Docker stuff
+echo "Installing docker ...."
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Adding user to docker group"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
